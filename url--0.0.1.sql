1e2ff6139457dd2573bdbdc60cf15d582fd2afcc
Lorenc1o <joseantoniolorencio@gmail.com>
2022-11-13 20:04:17 +0100

    Changed the constructor to use pointers instead of Datums. Still not working!!

--- url--0.0.1.sql
+++ url--0.0.1.sql
@@ -5,14 +5,34 @@ CREATE FUNCTION url_test(text) RETURNS text
     AS '$libdir/url'
     LANGUAGE C IMMUTABLE STRICT;
 
-CREATE TYPE url AS (
-    protocol text,
-    host text,
-    port int,
-    file text
+CREATE FUNCTION url_in(cstring)
+    RETURN url
+    AS '$libdir/url', 'url_in'
+    LANGUAGE C IMMUTABLE STRICT;
+
+CREATE FUNCTION url_out(url)
+    RETURN cstring
+    AS '$libdir/url', 'url_out'
+    LANGUAGE C IMMUTABLE STRICT;
+
+-- CREATE FUNCTION url_recv(internal)
+--     RETURN url
+--     AS '$libdir/url', 'url_recv'
+--     LANGUAGE C IMMUTABLE STRICT;
+
+-- CREATE FUNCTION url_send(url)
+--     RETURN bytea
+--     AS '$libdir/url', 'url_send'
+--     LANGUAGE C IMMUTABLE STRICT;
+
+CREATE TYPE url (
+    INPUT = url_in,
+    OUTPUT = url_out
+    -- RECEIVE = url_recv,
+    -- SEND = url_send
 );
 
-CREATE OR REPLACE FUNCTION url(protocol text, host text, port int, file text)
-RETURNS url
-AS '$libdir/url', 'url_constructor_all_fields'
-LANGUAGE C IMMUTABLE STRICT;
+--CREATE OR REPLACE FUNCTION url(protocol text, host text, port int, file text)
+--RETURNS url
+--AS '$libdir/url', 'url_constructor_all_fields'
+--LANGUAGE C IMMUTABLE STRICT;
--- url.c
+++ url.c
@@ -6,33 +6,93 @@
 
 PG_MODULE_MAGIC;
 
-struct url{
+typedef struct{
+  int32 length;
   char *protocol;
   char *host;
   int   port;
   char *file;
-};
+} Url;
 
-static inline struct url parse_url_from_str(const char *str){
+static inline Url *parse_url_from_str(const char *str){
+  char *protocol = "protocol"; //len 8
+  char *host = "host"; // len 4
+  int port = 80; // len 4
+  char *file = "file"; // len 4
+  Url *u = (Url *)palloc(VARHDRSZ + 8 + 4 + 4 + 4);
 
+  SET_VARSIZE(u, VARHDRSZ + 8 + 4 + 4 + 4);
+
+  memcpy(u->protocol, protocol, 8);
+  memcpy(u->host, host, 4);
+  u->port = port;
+  memcpy(u->file, file, 4);
+  PG_RETURN_POINTER(u);
+}
+
+PG_FUNCTION_INFO_V1(url_in);
+Datum
+url_in(PG_FUNCTION_ARGS)
+{
+  char *str = PG_GETARG_CSTRING(0);
+  Url *url = parse_url_from_str(str);
+  elog(INFO, "url_in: %s", str);
+  PG_RETURN_POINTER(url);
+}
+
+PG_FUNCTION_INFO_V1(url_out);
+Datum
+url_out(PG_FUNCTION_ARGS)
+{
+  Url *url = (Url *) PG_GETARG_POINTER(0);
+  char *str = palloc(1024);
+
+  sprintf(str, "%s://%s:%d%s", url->protocol, url->host, url->port, url->file);
+  PG_RETURN_CSTRING(str);
+}
+
+PG_FUNCTION_INFO_V1(url_rcv);
+Datum
+url_rcv(PG_FUNCTION_ARGS)
+{
+  StringInfo buf = (StringInfo) PG_GETARG_POINTER(0);
+  Url *url = palloc(sizeof(Url));
+  url->protocol = pstrdup(buf->data);
+  url->host = pstrdup(buf->data + strlen(url->protocol) + 1);
+  url->port = atoi(buf->data + strlen(url->protocol) + strlen(url->host) + 2);
+  url->file = pstrdup(buf->data + strlen(url->protocol) + strlen(url->host) + strlen(url->file) + 4);
+  PG_RETURN_POINTER(url);
+}
+
+PG_FUNCTION_INFO_V1(url_send);
+Datum
+url_send(PG_FUNCTION_ARGS)
+{
+  Url *url = (Url *) PG_GETARG_POINTER(0);
+  StringInfoData buf;
+  pq_begintypsend(&buf);
+  pq_sendbytes(&buf, url->protocol, strlen(url->protocol) + 1);
+  pq_sendbytes(&buf, url->host, strlen(url->host) + 1);
+  pq_sendbytes(&buf, url->port, 4 + 1);
+  pq_sendbytes(&buf, url->file, strlen(url->file) + 1);
+  PG_RETURN_BYTEA_P(pq_endtypsend(&buf));
 }
 
 PG_FUNCTION_INFO_V1(url_test);
 Datum
 url_test(PG_FUNCTION_ARGS)
-{
-    
-    text  *arg1 = cstring_to_text("http://www.google.com/");
-    text  *arg2 = PG_GETARG_TEXT_PP(0);
-    int32 arg1_size = VARSIZE_ANY_EXHDR(arg1);
-    int32 arg2_size = VARSIZE_ANY_EXHDR(arg2);
-    int32 new_text_size = arg1_size + arg2_size + VARHDRSZ;
-    text *new_text = (text *) palloc(new_text_size);
+{   
+  text  *arg1 = cstring_to_text("http://www.google.com/");
+  text  *arg2 = PG_GETARG_TEXT_PP(0);
+  int32 arg1_size = VARSIZE_ANY_EXHDR(arg1);
+  int32 arg2_size = VARSIZE_ANY_EXHDR(arg2);
+  int32 new_text_size = arg1_size + arg2_size + VARHDRSZ;
+  text *new_text = (text *) palloc(new_text_size);
 
-    SET_VARSIZE(new_text, new_text_size);
-    memcpy(VARDATA(new_text), VARDATA_ANY(arg1), arg1_size);
-    memcpy(VARDATA(new_text) + arg1_size, VARDATA_ANY(arg2), arg2_size);
-    PG_RETURN_TEXT_P(new_text);
+  SET_VARSIZE(new_text, new_text_size);
+  memcpy(VARDATA(new_text), VARDATA_ANY(arg1), arg1_size);
+  memcpy(VARDATA(new_text) + arg1_size, VARDATA_ANY(arg2), arg2_size);
+  PG_RETURN_TEXT_P(new_text);
 }
 
 //TODO: This is not working. How to return multiple values?
